aaec6116ce92654ec65fdee09c6bb67727b77aba
Silviu Caragea <silviu.cpp@gmail.com>
2017-01-16 17:14:33 +0200

    Fix compiling script

--- c_src/env.mk
+++ c_src/env.mk
@@ -1,3 +1,3 @@
-ERTS_INCLUDE_DIR ?= /usr/local/lib/erlang/erts-8.0/include/
-ERL_INTERFACE_INCLUDE_DIR ?= /usr/local/lib/erlang/lib/erl_interface-3.9/include
-ERL_INTERFACE_LIB_DIR ?= /usr/local/lib/erlang/lib/erl_interface-3.9/lib
+ERTS_INCLUDE_DIR ?= /usr/local/lib/erlang/erts-8.2/include/
+ERL_INTERFACE_INCLUDE_DIR ?= /usr/local/lib/erlang/lib/erl_interface-3.9.2/include
+ERL_INTERFACE_LIB_DIR ?= /usr/local/lib/erlang/lib/erl_interface-3.9.2/lib
